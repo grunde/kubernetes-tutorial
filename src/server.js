96d14296965f1afc6937a676af64da55f2cbb399
grunde <etan.gru@gmail.com>
2019-09-12 18:31:05 +0300

    update server code

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const http = require('http');
-
+let requests = 0;
+const podname = process.env.HOSTNAME;
+let startTime;
+let host;
 const handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
+  response.setHeader('Content-Type', 'text/plain');
   response.writeHead(200);
-  response.end('Hello World!');
+  response.write("Hello Kubernetes bootcamp! | Running on: ");
+  response.write(host);
+  response.end(" | v=1\n");
+  console.log("Running On:", host, "| Total Requests:", ++requests, "| App Uptime:", (new Date() - startTime) / 1000, "seconds", "| Log Time:", new Date());
 };
-
 const www = http.createServer(handleRequest);
-www.listen(8080);
+www.listen(8080,function () {
+  startTime = new Date();
+  host = process.env.HOSTNAME;
+  console.log ("Kubernetes Bootcamp App Started At:",startTime, "| Running On: " ,host, "\n" );
+});
